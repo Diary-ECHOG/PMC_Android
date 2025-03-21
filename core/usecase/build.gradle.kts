fb391ef074ca532c3caa70c70e1a986b437a70aa
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2025-03-22 08:44:31 +0900

    dependency수정

--- core/usecase/build.gradle.kts
+++ core/usecase/build.gradle.kts
@@ -1,79 +1,56 @@
-//plugins {
-//    alias(libs.plugins.androidLibrary)
-//    alias(libs.plugins.jetbrainsKotlinAndroid)
-//    alias(libs.plugins.ksp)
-//    alias(libs.plugins.hilt.android)
-//    kotlin("plugin.serialization") version "2.1.10"
-//}
-//
-//android {
-//    namespace = "com.app.pmc.core.usecase"
-//    compileSdk = 34
-//
-//    defaultConfig {
-//        minSdk = 24
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        consumerProguardFiles("consumer-rules.pro")
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
-//    kotlinOptions {
-//        jvmTarget = "11"
-//    }
-//}
-//
-//dependencies {
-//    implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.appcompat)
-//    implementation(project(":data"))
-//    implementation(project(":core:repository"))
-//    implementation(project(":core:repository"))
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//
-//    //hilt
-//    implementation(libs.hilt.android)
-//    implementation(libs.androidx.hilt.navigation.compose)
-//    implementation(libs.androidx.lifecycle.viewmodel.android)
-//    ksp(libs.hilt.compiler)
-//    ksp(libs.androidx.hilt.complier)
-//
-//    implementation(project(":core:model"))
-//}
-
-
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
+android {
+    namespace = "com.app.pmc.core.usecase"
+    compileSdk = 34
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
 dependencies {
-    implementation(libs.hilt.core)
-    implementation(project(":core:model"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(project(":data"))
     implementation(project(":core:repository"))
+    implementation(project(":core:repository"))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.android)
+    ksp(libs.hilt.compiler)
+    ksp(libs.androidx.hilt.complier)
+
+    implementation(project(":core:model"))
 }
